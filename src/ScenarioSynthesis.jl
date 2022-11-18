033bbc44579d8c9538d40fff4133516bb57f1480
Florian Finkeldei <florian.finkeldei@tum.de>
2022-11-18 09:28:16 +0100

    initialized julia package structure

---
+++ src/ScenarioSynthesis.jl
@@ -0,0 +1,5 @@
+module ScenarioSynthesis
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ScenarioSynthesis
+using Test
+
+@testset "ScenarioSynthesis.jl" begin
+    # Write your tests here.
+end
