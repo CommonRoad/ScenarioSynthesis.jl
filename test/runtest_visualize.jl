91ad8e39e26e593b01b35ce7de173fb4d45d63b8
Florian Finkeldei <florian.finkeldei@tum.de>
2022-12-15 10:57:12 +0100

    added runtests for plots

---
+++ test/runtest_visualize.jl
@@ -0,0 +1,22 @@
+using ScenarioSynthesis
+using Test
+import Plots
+
+@testset "plots" begin
+    path = joinpath(@__DIR__, "..", "example_files", "DEU_Cologne-9_6_I-1.cr.xml")
+    ln = ln_from_xml(path)
+    process!(ln)
+
+    p_lt = plot_lanelet(ln.lanelets[64], 64)
+    @test isa(p_lt, Plots.Plot)
+
+    p_ln = plot_lanelet_network(ln)
+    @test isa(p_ln, Plots.Plot)
+
+    p_po = plot_polygon(Polygon(FCart, [1 2 3; 4 5 6]))
+    @test isa(p_po, Plots.Plot)
+
+    route = Route(LaneletID.([64]), ln)
+    p_ro = plot_route(route)
+    @test isa(p_ro, Plots.Plot)
+end
